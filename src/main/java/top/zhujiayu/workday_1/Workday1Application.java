9837196d1d2d9957e25f447f6f29b7ed2723607f
zhujiayu <457147489@qq.com>
2020-01-14 11:22:19 +0800

    Initial commit

---
+++ src/main/java/top/zhujiayu/workday_1/Workday1Application.java
@@ -0,0 +1,13 @@
+package top.zhujiayu.workday_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Workday1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Workday1Application.class, args);
+    }
+
+}
---
+++ src/test/java/top/zhujiayu/workday_1/Workday1ApplicationTests.java
@@ -0,0 +1,13 @@
+package top.zhujiayu.workday_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Workday1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
